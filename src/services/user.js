40af01a735a8e2bf9f579a0b40539a1e9ffbe90e
Jacegk1999 <Jacegk1999@gmail.com>
2022-04-07 10:43:29 -0700

    supabase services

---
+++ src/services/user.js
@@ -0,0 +1,21 @@
+import { client, checkError } from './client.js';
+
+export function getUser() {
+  return client.auth.session() && client.auth.session().user.email;
+}
+
+export async function signupUser(email, password) {
+  const { user } = await client.auth.signUp({ email, password });
+
+  return user;
+}
+
+export async function signInUser(email, password) {
+  const { user } = await client.auth.signIn({ email, password });
+  return user;
+}
+
+export async function logout() {
+  const response = await client.auth.signOut();
+  return checkError(response);
+}
