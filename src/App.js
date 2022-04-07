2edc9d6d6ec2eb3e347c57e22923755892e50d7b
Jacegk1999 <Jacegk1999@gmail.com>
2022-04-07 12:42:40 -0700

    have some routing set up for testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import Header from './Views/Header/Header';
 import { Auth } from './Views/Auth/Auth';
+import { BrowserRouter } from 'react-router-dom';
+import { getUser } from './services/user';
+import { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Trial from './Views/Trial';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(getUser());
   return (
     <div className="App">
-      <Header />
-      <Auth />
+      <BrowserRouter>
+        <Header currentUser={currentUser} />
+        <Route exact path={'/'}>
+          {!currentUser ? <Auth setCurrentUser={setCurrentUser} /> : <Redirect to={'/trial'} />}
+        </Route>
+        <Route path={'/trial'}>
+          <Trial />
+        </Route>
+      </BrowserRouter>
     </div>
   );
 }
